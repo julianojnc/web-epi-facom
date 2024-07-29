c4df9cafef6ddce48e81ec9be7c97865e84d3af8
Juliano do Nascimento da Costa <juliano.nascimento@FACOM.LOCAL.BR>
2024-07-29 09:43:23 -0300

    Cadastro de EPi com listagem de Marcas. Cadastro de Usuarios

--- src/Pages/PageEpi/CadastrarEpi/index.js
+++ src/Pages/PageEpi/CadastrarEpi/index.js
@@ -1,13 +1,123 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MenuBar from "../../../componentes/MenuBar";
 import ModalManutencaoEpi from "./ModalManutencaoEpi";
 import iconLink from "../../../assets/icon-link.png"
 import iconManutencao from "../../../assets/icon-manutencao.png"
+import axios from "axios";
 
 const CadastrarEpi = () => {
 
     const [modalOpen, setModalOpen] = useState(false);
+
+    const epi = {
+        nome: '',
+        patrimonio: '',
+        local: '',
+        setor: '',
+        dataCompra: '',
+        dataGarantia: '',
+        idMarca: {
+            id: '',
+        }
+    };
+
+    const [epis, setEpis] = useState([]);
+    const [objEpi, setObjEpi] = useState(epi);// Funcao para o cadastro de Epi
+
+    const objMarcas = { id: "" };
+    const [marcas, setMarcas] = useState([]);
+    const [objMarca, setObjMarca] = useState(objMarcas);
+    const [searchMarca, setSearchMarca] = useState('');
+    const [searchMarcaOpen, setSearchMarcaOpen] = useState(false);
+    const [carregandoMarcas, setCarregandoMarcas] = useState(true);
+
+    useEffect(() => {
+        const fetchMarcas = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/marca');
+                if (Array.isArray(response.data.lista)) {
+                    setMarcas(response.data.lista);
+                } else {
+                    console.error('A resposta da API não contém a estrutura esperada:', response.data.lista);
+                }
+            } catch (error) {
+                if (error.response) {
+                    // A requisição foi feita e o servidor respondeu com um status code fora do alcance 2xx
+                    console.error('Erro na resposta da API:', error.response);
+                } else if (error.request) {
+                    // A requisição foi feita mas nenhuma resposta foi recebida
+                    console.error('Erro na requisição:', error.request);
+                } else {
+                    // Algo aconteceu ao configurar a requisição que disparou um erro
+                    console.error('Erro ao configurar a requisição:', error.message);
+                }
+            }
+        };
+        fetchMarcas();
+    }, []);
+
+    useEffect(() => {
+        if (marcas.length === 0) {
+            setCarregandoMarcas(true);
+        } else {
+            setCarregandoMarcas(false);
+        }
+    }, [marcas]);
+
+    const filterData = (data, searchTerm, fields) => {
+        return data.filter(item =>
+            fields.some(field => item[field].toLowerCase().includes(searchTerm.toLowerCase()))
+        );
+    };
+
+    const handleSelection = (type, item) => {
+        if (type === 'marca') {
+            setObjMarca(item);
+            setSearchMarca(item.nome);
+            setSearchMarcaOpen(false);
+            setObjEpi(prevState => ({
+                ...prevState,
+                idMarca: item
+            }));
+        }
+    };
+
+    const cadastrar = () => {
+        console.log('Objeto a ser enviado:', objEpi); // Adicione esta linha para verificar o objeto
+
+        fetch('http://localhost:4000/api/epi', {
+            method: 'post',
+            body: JSON.stringify(objEpi),
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(retorno => {
+                if (!retorno.ok) {
+                    throw new Error(`HTTP error! status: ${retorno.status}`);
+                }
+                return retorno.json();
+            })
+            .then(retorno_convertido => {
+                console.log('Resposta da API:', retorno_convertido); // Adicione esta linha para verificar a resposta
+
+                if (retorno_convertido.mensagem !== undefined) {
+                    alert(retorno_convertido.mensagem);
+                } else {
+                    setEpis([...epis, retorno_convertido]);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao cadastrar Epi:', error);
+                alert('Ocorreu um erro ao tentar cadastrar Epi. Confira se não há duplicidade!');
+            });
+    };
+
+    const aoDigitar = (e) => {
+        setObjEpi({ ...objEpi, [e.target.name]: e.target.value });
+    }
 
     // Modal Open
     const closeModal = () => {
@@ -46,16 +156,58 @@ const CadastrarEpi = () => {
 
                 <form>
                     <label className="label"> Nome:
-                        <input className="input" type="text" placeholder="Nome" />
+                        <input
+                            value={objEpi.nome}
+                            onChange={aoDigitar}
+                            name='nome'
+                            className="input"
+                            type="text"
+                            placeholder="Nome"
+                        />
                     </label>
 
                     <label className="label"> Patrimônio:
-                        <input className="input" type="text" placeholder="Patrimônio" />
+                        <input
+                            value={objEpi.patrimonio}
+                            onChange={aoDigitar}
+                            name='patrimonio'
+                            className="input"
+                            type="text"
+                            placeholder="Patrimônio" />
                     </label>
 
                     <div className="marca-checkbox">
+
+                        <input
+                            value={objMarca.codMarca}
+                            onChange={aoDigitar}
+                            name="codMarca.id"
+                            className="input input-marca"
+                            type="text"
+                            placeholder="ID Marca"
+                            hidden />
+
                         <label className="label"> Marca:
-                            <input className="input input-marca" type="text" placeholder="Marca" />
+                            <input
+                                value={searchMarca}
+                                onClick={() => setSearchMarcaOpen(true)}
+                                onChange={(e) => setSearchMarca(e.target.value)}
+                                name="idMarca.id"
+                                className="input input-marca"
+                                type="text"
+                                placeholder="Marca" />
+                                
+                                {searchMarcaOpen && (
+                                        <div className="search-bar-curso">
+                                            {filterData(marcas, searchMarca, ['nome']).map((obj, indice) => (
+                                                <ul key={indice} onClick={() => handleSelection('marca', obj)}>
+                                                    <li>
+                                                        <p>{obj.nome}</p>
+                                                    </li>
+                                                </ul>
+                                            ))}
+                                        </div>
+                                    )}
                         </label>
 
                         <label className="label label-details">
@@ -65,43 +217,82 @@ const CadastrarEpi = () => {
                     </div>
 
                     <label className="label"> Service Tag:
-                        <input className="input" type="text" placeholder="Service Tag" />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Service Tag" />
                     </label>
 
                     <label className="label"> Express Code:
-                        <input className="input" type="text" placeholder="Express Code" />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Express Code" />
                     </label>
 
                     <label className="label"> Local:
-                        <input className="input" type="text" placeholder="Local" />
+                        <input
+                            value={objEpi.local}
+                            onChange={aoDigitar}
+                            name='local'
+                            className="input"
+                            type="text"
+                            placeholder="Local" />
                     </label>
 
                     <label className="label"> Setor:
-                        <input className="input" type="text" placeholder="Setor" />
+                        <input
+                            value={objEpi.setor}
+                            onChange={aoDigitar}
+                            name='setor'
+                            className="input"
+                            type="text"
+                            placeholder="Setor" />
                     </label>
 
                     <label className="label"> Data da Compra:
-                        <input className="input" type="text" placeholder="Data da Compra" />
+                        <input
+                            value={objEpi.dataCompra}
+                            onChange={aoDigitar}
+                            name='dataCompra'
+                            className="input"
+                            type="date"
+                            placeholder="Data da Compra" />
                     </label>
 
                     <label className="label"> Data de Vencimento:
-                        <input className="input" type="text" placeholder="Data de Vencimento da Garantia" />
+                        <input
+                            value={objEpi.dataGarantia}
+                            onChange={aoDigitar}
+                            name='dataGarantia'
+                            className="input"
+                            type="date"
+                            placeholder="Data de Vencimento da Garantia" />
                     </label>
 
                     <label className="label"> Usuario
-                        <input className="input" type="text" placeholder="Usuario" />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Usuario" />
                     </label>
 
                     <label className="label"> Email do Usuario:
-                        <input className="input" type="text" placeholder="Email" />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Email" />
                     </label>
 
                     <label className="label"> Contato do Usuario:
-                        <input className="input" type="text" placeholder="Contato" />
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Contato" />
                     </label>
 
                     <div className="container-buttons">
-                        <Link to='/cadastro-epi' className="button button-cadastrar">Cadastrar</Link>
+                        <Link to='/cadastro-epi' onClick={cadastrar} className="button button-cadastrar">Cadastrar</Link>
                         <Link to='/cadastro-epi' hidden className="button button-cadastrar alterar">Alterar</Link>
                         <Link to='/cadastro-epi' hidden className="button button-cadastrar excluir">Excluir</Link>
                     </div>
--- src/Pages/PageUsers/CadastrarUsers/index.js
+++ src/Pages/PageUsers/CadastrarUsers/index.js
@@ -2,8 +2,55 @@ import MenuBar from "../../../componentes/MenuBar";
 import { Link } from "react-router-dom";
 import iconLink from "../../../assets/icon-link.png"
 import iconManutencao from "../../../assets/icon-manutencao.png"
+import { useState } from "react";
 
 const CadastrarUsers = () => {
+
+    const user = {
+        nome: '',
+        email: '',
+        telContato: ''
+    };
+
+    const [users, setUsers] = useState([]);
+    const [objUser, setObjUser] = useState(user);// Funcao para o cadastro de Usuarios
+
+    const cadastrar = () => {
+        console.log('Objeto a ser enviado:', objUser); // Adicione esta linha para verificar o objeto
+
+        fetch('http://localhost:4000/api/usuarios', {
+            method: 'post',
+            body: JSON.stringify(objUser),
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(retorno => {
+                if (!retorno.ok) {
+                    throw new Error(`HTTP error! status: ${retorno.status}`);
+                }
+                return retorno.json();
+            })
+            .then(retorno_convertido => {
+                console.log('Resposta da API:', retorno_convertido); // Adicione esta linha para verificar a resposta
+
+                if (retorno_convertido.mensagem !== undefined) {
+                    alert(retorno_convertido.mensagem);
+                } else {
+                    setUsers([...users, retorno_convertido]);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao cadastrar a marca:', error);
+                alert('Ocorreu um erro ao tentar cadastrar Usuario.');
+            });
+    };
+
+    const aoDigitar = (e) => {
+        setObjUser({ ...objUser, [e.target.name]: e.target.value });
+    }
+
     return (
         <section>
             <MenuBar />
@@ -36,20 +83,37 @@ const CadastrarUsers = () => {
 
                 <form>
                     <label className="label"> Nome:
-                        <input className="input" type="text" placeholder="Nome" />
+                        <input
+                            value={objUser.nome}
+                            onChange={aoDigitar}
+                            name='nome'
+                            className="input"
+                            type="text"
+                            placeholder="Nome" />
+                    </label>
+
+                    <label className="label"> Contato:
+                        <input
+                            value={objUser.telContato}
+                            onChange={aoDigitar}
+                            name='telContato'
+                            className="input"
+                            type="text"
+                            placeholder="Contato" />
                     </label>
 
                     <label className="label"> Email:
-                        <input className="input" type="text" placeholder="Email" />
-                    </label>
-
-
-                    <label className="label"> Contato:
-                        <input className="input" type="text" placeholder="Contato" />
+                        <input
+                            value={objUser.email}
+                            onChange={aoDigitar}
+                            name='email'
+                            className="input"
+                            type="text"
+                            placeholder="Email" />
                     </label>
 
                     <div className="container-buttons">
-                        <Link to='/cadastro-epi' className="button button-cadastrar">Cadastrar</Link>
+                        <Link onClick={cadastrar} className="button button-cadastrar">Cadastrar</Link>
                         <Link to='/cadastro-epi' hidden className="button button-cadastrar alterar">Alterar</Link>
                         <Link to='/cadastro-epi' hidden className="button button-cadastrar excluir">Excluir</Link>
                     </div>
