0bd6b09121b0da9775d3ff9407ac6e1a45bb8afe
Juliano do Nascimento da Costa <juliano.nascimento@FACOM.LOCAL.BR>
2024-08-15 09:52:49 -0300

    Nova pagina NotFound adicionada

--- src/Pages/PageNotFound/index.js
+++ src/Pages/PageNotFound/index.js
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+import MenuBar from "../../componentes/MenuBar";
+import imgSemConexao from "../../assets/img-sem-conexao.png"
+
 const PageNotFound = () => {
     return (
         <section>
+            <MenuBar />
 
-            <h1>Pagina não encontrada</h1>
-            
+            <div className="content-page">
+                <main className="page-not-found">
+                    <div>
+                        <h1>Erro 404! <br></br>Pagina não encontrada!</h1>
+                        <Link to="/" className="button button-cadastrar">Voltar</Link>
+                    </div>
+                    <span>
+                        <img src={imgSemConexao} alt="img" />
+                    </span>
+                </main>
+            </div>
         </section>
     )
 }
