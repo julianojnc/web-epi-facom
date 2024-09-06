56b1ec668d8d00ebf448c42b31e6d5610b2350f2
Juliano do Nascimento da Costa <juliano.nascimento@FACOM.LOCAL.BR>
2024-09-06 09:04:32 -0300

    ao cadastrar um novo usuario na modal de vincular usuario ele e retornado pela api para os campos do input

--- src/Pages/PageEpi/CadastrarEpi/ModalVincularUsuario/index.js
+++ src/Pages/PageEpi/CadastrarEpi/ModalVincularUsuario/index.js
@@ -4,10 +4,11 @@ import { useState } from "react";
 import iconClose from "../../../../assets/icon-close.png"
 import { cadastrarUsers } from "../../../PageUsers/api/apiUser";
 import TableVincularUsuario from "./TableVincularUsuario";
+import ModalSucess from "../../../../componentes/Modal/ModalSucess";
 
 const ModalVincularUsuario = ({ onClose, objEpi }) => {
 
-    const user = {
+    const initialUser = {
         nome: '',
         email: '',
         telContato: ''
@@ -16,33 +17,40 @@ const ModalVincularUsuario = ({ onClose, objEpi }) => {
     const [vincularUsuarioPergunta, setVincularUsuarioPergunta] = useState(true);
     const [vincularUsuario, setVincularUsuario] = useState(false);
     const [users, setUsers] = useState([]);
-    const [objUser, setObjUser] = useState(user);// Funcao para o cadastro de Usuarios
-
-
+    const [objUser, setObjUser] = useState(initialUser); // Função para o cadastro de Usuários
+    const [sucessAnimation, setSucessAnimation] = useState(false); // Modal Cadastrado com Sucesso
+    
     const cadastrarVinculoUsuario = () => {
         setVincularUsuario(true);
-        setVincularUsuarioPergunta(false)
-    }
+        setVincularUsuarioPergunta(false);
+    };
 
     const cadastrar = async () => {
         console.log('Objeto a ser enviado:', objUser);
         try {
             const response = await cadastrarUsers(objUser);
             console.log('Resposta da API:', response);
+
             if (response.mensagem) {
                 alert(response.mensagem);
             } else {
-                setUsers([...users, response]);
+                // // Atualizar o estado com os dados do usuário cadastrado
+                setSucessAnimation(true); // Modal Cadastrado com sucesso é ativada
+                setTimeout(() => { // Tempo de 2segundos é disparado
+                    setSucessAnimation(false); // Modal Cadastrado com sucesso é desativada
+                    setObjUser(response); // Preenche os inputs com os dados retornados da API
+                    setUsers([...users, response]);
+                }, 2000 /* Declarado os 2 segundos */);
             }
         } catch (error) {
             console.error('Erro ao cadastrar Usuario:', error);
-            alert('Ocorreu um erro ao tentar cadastrar Usuario. Confira se não há duplicidade!');
+            alert('Ocorreu um erro ao tentar cadastrar Usuario!');
         }
     };
 
     const aoDigitar = (e) => {
         setObjUser({ ...objUser, [e.target.name]: e.target.value });
-    }
+    };
 
     return (
         <Modal>
@@ -51,94 +59,102 @@ const ModalVincularUsuario = ({ onClose, objEpi }) => {
                     <h1>Deseja Vincular um Usuário ao Equipamento: {objEpi.patrimonio}?</h1>
 
                     <div className="buttonsModal">
-                        <Link className="buttonModalYes" onClick={() => cadastrarVinculoUsuario()}>Sim</Link>
-                        <Link className="buttonModalNo" onClick={() => onClose()}>Não</Link>
+                        <Link className="buttonModalYes" onClick={cadastrarVinculoUsuario}>Sim</Link>
+                        <Link className="buttonModalNo" onClick={onClose}>Não</Link>
                     </div>
                 </>
             )}
-            {
-                vincularUsuario && (
-                    <>
-                        <div className="dialog-title">
-                            <h1>Vincular Usuário</h1>
+            {vincularUsuario && (
+                <>
+                    <div className="dialog-title">
+                        <h1>Vincular Usuário</h1>
 
-                            <Link title="Fechar Modal" onClick={() => onClose()}>
-                                <span>
-                                    <img src={iconClose} alt="icon"></img>
-                                </span>
-                            </Link>
-                        </div>
+                        <Link title="Fechar Modal" onClick={onClose}>
+                            <span>
+                                <img src={iconClose} alt="icon" />
+                            </span>
+                        </Link>
+                    </div>
 
-                        <div className="dialog-content">
-                            <form>
-                                <label className="label"> Pesquisar Usuario:
-                                    <input className="input" type="text" placeholder="Pesquisar Usuários Existentes..." />
-                                </label>
+                    <div className="dialog-content">
+                        <form>
+                            <label className="label"> Pesquisar Usuario:
+                                <input className="input" type="text" placeholder="Pesquisar Usuários Existentes..." />
+                            </label>
 
+                            <input
+                                value={objEpi.id}
+                                onChange={aoDigitar}
+                                name='objEpi.id'
+                                className="input"
+                                type="text"
+                                placeholder="Id Epi"
+                                hidden
+                            />
+
+                            <input
+                                value={objUser.id || ''}
+                                onChange={aoDigitar}
+                                name='objUser.id'
+                                className="input"
+                                type="text"
+                                placeholder="Id Usuario"
+                                hidden
+                            />
+
+                            <label className="label"> Nome:
                                 <input
-                                    value={objEpi.id}
+                                    value={objUser.nome}
                                     onChange={aoDigitar}
-                                    name='objEpi.id'
+                                    name='nome'
                                     className="input"
                                     type="text"
-                                    placeholder="Id Epi"
-                                    hidden
+                                    placeholder="Nome"
                                 />
+                            </label>
 
+                            <label className="label"> Contato:
                                 <input
-                                    value={objUser.id}
+                                    value={objUser.telContato}
                                     onChange={aoDigitar}
-                                    name='objUser.id'
+                                    name='telContato'
                                     className="input"
                                     type="text"
-                                    placeholder="Id Usuario"
-                                    hidden
+                                    placeholder="Contato"
                                 />
+                            </label>
 
-                                <label className="label"> Nome:
-                                    <input
-                                        value={objUser.nome}
-                                        onChange={aoDigitar}
-                                        name='nome'
-                                        className="input"
-                                        type="text"
-                                        placeholder="Nome" />
-                                </label>
+                            <label className="label"> Email:
+                                <input
+                                    value={objUser.email}
+                                    onChange={aoDigitar}
+                                    name='email'
+                                    className="input"
+                                    type="text"
+                                    placeholder="Email"
+                                />
+                            </label>
 
-                                <label className="label"> Contato:
-                                    <input
-                                        value={objUser.telContato}
-                                        onChange={aoDigitar}
-                                        name='telContato'
-                                        className="input"
-                                        type="text"
-                                        placeholder="Contato" />
-                                </label>
+                            <div className="container-buttons">
+                                <Link onClick={cadastrar} className="button button-cadastrar">Cadastrar Novo</Link>
+                                <Link to='/cadastro-epi' className="button button-cadastrar">Vincular</Link>
+                            </div>
+                        </form>
 
-                                <label className="label"> Email:
-                                    <input
-                                        value={objUser.email}
-                                        onChange={aoDigitar}
-                                        name='email'
-                                        className="input"
-                                        type="text"
-                                        placeholder="Email" />
-                                </label>
+                        <TableVincularUsuario />
+                    </div>
 
-                                <div className="container-buttons">
-                                    <Link onClick={cadastrar} className="button button-cadastrar">Cadastrar Novo</Link>
-                                    <Link to='/cadastro-epi' className="button button-cadastrar">Vincular</Link>
-                                </div>
-                            </form>
-
-                            <TableVincularUsuario />
-
-                        </div>
-                    </>
-                )
-            }
+                    {sucessAnimation && (
+                        <ModalSucess
+                            id={objUser.id}
+                            title="Usuário Cadastrado!"
+                            titleEditar="Usuário Vinculado!"
+                        />
+                    )}
+                </>
+            )}
         </Modal>
-    )
-}
+    );
+};
 
 export default ModalVincularUsuario;
